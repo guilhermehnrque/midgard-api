a659ca1dd8deeff3548d8f260002596c1980a893
Guilherme Pereira <guilhermehnrque@gmail.com>
2024-10-21 23:14:04 -0300

    feat: atualizando validadores

--- src/infrastructure/routes/players/GroupMembershipRoutes.ts
+++ src/infrastructure/routes/players/GroupMembershipRoutes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import { GroupMembershipController } from "../../controllers/player/GroupMembershipController";
-import { schemas, handleValidationErrors } from "../../middlewares/validators/players/GroupMembershipValidator";
+import { handleValidationErrors } from "../../middlewares/validators/auth/AuthValidator";
+import { getGroupDetailsValidation, getMembersValidation, joinGroupValidation, leaveGroupValidation } from "../../middlewares/validators/players/GroupMembershipValidator";
+
 
 export class GroupMembershipRouter {
 
@@ -14,11 +16,11 @@ export class GroupMembershipRouter {
     }
 
     private initializeRoutes() {
-        this.router.post('', [...schemas.joinGroup, handleValidationErrors.handle], (req: Request, res: Response) =>
+        this.router.post('', joinGroupValidation, handleValidationErrors.handle, (req: Request, res: Response) =>
             this.groupMembershipController.joinGroup(req, res)
         );
 
-        this.router.delete('', [...schemas.leaveGroup, handleValidationErrors.handle], (req: Request, res: Response) =>
+        this.router.delete('', leaveGroupValidation, handleValidationErrors.handle, (req: Request, res: Response) =>
             this.groupMembershipController.leaveGroup(req, res)
         );
 
@@ -26,10 +28,10 @@ export class GroupMembershipRouter {
             this.groupMembershipController.getGroups(req, res)
         );
 
-        this.router.get('/group/:groupId', [...schemas.getMembers, handleValidationErrors.handle], (req: Request, res: Response) =>
+        this.router.get('/group/:groupId', getMembersValidation, handleValidationErrors.handle, (req: Request, res: Response) =>
             this.groupMembershipController.getGroupMembership(req, res)
         );
-        this.router.get('/group/:groupId/details', [...schemas.getGroupDetails, handleValidationErrors.handle], (req: Request, res: Response) =>
+        this.router.get('/group/:groupId/details', getGroupDetailsValidation, handleValidationErrors.handle, (req: Request, res: Response) =>
             this.groupMembershipController.getGroupDetails(req, res)
         );
     }
--- src/infrastructure/routes/players/ListMembershipRoutes.ts
+++ src/infrastructure/routes/players/ListMembershipRoutes.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import { ListMembershipController } from "../../controllers/player/ListMembershipController";
+import { validateGetJoinedLists, validateGetLists, validateJoinList, validateLeaveList } from "../../middlewares/validators/players/ListMembershipValidator";
+import { handleValidationErrors } from "../../middlewares/validators/auth/AuthValidator";
 
 export class ListMembershipRoutes {
     
@@ -13,9 +15,9 @@ export class ListMembershipRoutes {
     }
 
     private initializeRoutes(): void {
-        this.router.get("/groups/:groupId/lists", (req: Request, res: Response) => this.listMembershipController.getLists(req, res));
-        this.router.get("/joined-lists", (req: Request, res: Response) => this.listMembershipController.getJoinedLists(req, res));
-        this.router.post("/", (req: Request, res: Response) => this.listMembershipController.joinList(req, res));
-        this.router.delete("/", (req: Request, res: Response) => this.listMembershipController.leaveList(req, res));
+        this.router.get("/groups/:groupId/lists", validateGetLists, handleValidationErrors.handle, (req: Request, res: Response) => this.listMembershipController.getLists(req, res));
+        this.router.get("/joined-lists", validateGetJoinedLists, handleValidationErrors.handle, (req: Request, res: Response) => this.listMembershipController.getJoinedLists(req, res));
+        this.router.post("/", validateJoinList, handleValidationErrors.handle,(req: Request, res: Response) => this.listMembershipController.joinList(req, res));
+        this.router.delete("/", validateLeaveList, handleValidationErrors.handle, (req: Request, res: Response) => this.listMembershipController.leaveList(req, res));
     }
 }
